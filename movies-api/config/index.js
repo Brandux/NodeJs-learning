5138c9d2de12cc43cbaee8c40d73cd350eab62ea
Brandux Juarez Avila <branduxjuarez@upeu.edu.pe>
2020-08-21 00:06:08 -0500

    advance

--- movies-api/config/index.js
+++ movies-api/config/index.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 
 const config = {
     dev: true,
-    port:  3000
+    port:  3000,
+    cors = '*',
+    dbUser = process.env.DB_USER,
+    dbPassword = process.env.DB_PASSWORD,
+    dbHost = process.env.DB_HOST,
+    dbName = process.env.DB_NAME
 }
 
 module.exports = { config }
